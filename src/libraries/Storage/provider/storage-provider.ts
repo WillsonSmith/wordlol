c1d0296b01b6d8a0cba91b2fb7951084a02dfefa
Willson Smith <me@willsonsmith.com>
2023-03-25 15:47:52 -0400

    Initial roughin for a storage provider

---
+++ src/libraries/Storage/provider/storage-provider.ts
@@ -0,0 +1,26 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { provide } from '@lit-labs/context';
+import { storage } from '../context/storageContext';
+import { Storage } from '../Storage';
+
+@customElement(`storage-provider`)
+export class StorageProvider extends LitElement {
+  @provide({ context: storage })
+  @property({ type: Object })
+  storage: Storage = new Storage({
+    strategy: 'localstorage',
+  });
+
+  render() {
+    return html`<slot></slot>`;
+  }
+
+  static styles = [css``];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'storage-provider': StorageProvider;
+  }
+}
