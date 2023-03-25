966ebdec1928ff51a6bd71bcdb64499ef35c69b2
Willson Smith <me@willsonsmith.com>
2023-03-25 03:22:27 -0400

    Start function

---
+++ functions/search.ts
@@ -0,0 +1,34 @@
+import { OpenAI } from '../src/libraries/OpenAI';
+
+import { Handler } from '@netlify/functions';
+export const handler: Handler = async (event, context) => {
+  try {
+    const word = event.queryStringParameters?.search;
+    if (!word) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing search query' }),
+      };
+    }
+
+    const openAI = new OpenAI(process.env.OPENAI_API_KEY);
+    const response = await openAI.chatCompletion({
+      messages: [
+        {
+          role: 'user',
+          content: `Invent a new definition for "${word}" in the style of Urban Dictionary.`,
+        },
+      ],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
